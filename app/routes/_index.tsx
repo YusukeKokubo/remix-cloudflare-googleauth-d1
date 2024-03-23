58c076e003e9c3824a398def8b0ec8116175d58f
YusukeKokubo <yusuke.kokubo@gmail.com>
2024-03-23 18:46:27 +0900

    use authenticator

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,46 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import {
+  json,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/cloudflare"
+import { Form, useLoaderData } from "@remix-run/react"
+import authenticator from "~/libs/remix-auth/auth.server"
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
-    {
-      name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
-    },
-  ];
-};
+    { name: "description", content: "Welcome to Remix!" },
+  ]
+}
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const user = await authenticator.isAuthenticated(request)
+  return json({ user })
+}
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const { user } = useLoaderData<typeof loader>()
+  if (user) {
+    return (
+      <>
+        <div>
+          hello
+          {user.name}
+          <img src={user.image} alt={user.name} />
+        </div>
+
+        <Form action="/api/auth/signout" method="post">
+          <button type="submit">サインアウト</button>
+        </Form>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Form action="/api/auth/signin" method="post">
+          <button type="submit">サインイン</button>
+        </Form>
+      </>
+    )
+  }
 }
