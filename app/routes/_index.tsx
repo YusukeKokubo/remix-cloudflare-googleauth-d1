70ba72a88506191fb7d79a437a5a403339f5116c
YusukeKokubo <yusuke.kokubo@gmail.com>
2024-04-28 18:30:31 +0900

    show users list

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,6 +4,8 @@ import {
   json,
 } from "@remix-run/cloudflare"
 import { Form, useLoaderData } from "@remix-run/react"
+import { drizzle } from "drizzle-orm/d1"
+import { users } from "~/db/schema"
 import authenticator from "~/libs/remix-auth/auth.server"
 
 export const meta: MetaFunction = () => {
@@ -15,11 +17,14 @@ export const meta: MetaFunction = () => {
 
 export async function loader({ request, context }: LoaderFunctionArgs) {
   const user = await authenticator.isAuthenticated(request)
-  return json({ user })
+
+  const db = drizzle(context.cloudflare.env.DB, { schema: { users } })
+  const usersData = await db.query.users.findMany()
+  return json({ user, usersData })
 }
 
 export default function Index() {
-  const { user } = useLoaderData<typeof loader>()
+  const { user, usersData } = useLoaderData<typeof loader>()
   if (!user) {
     return (
       <>
@@ -35,6 +40,19 @@ export default function Index() {
         hello
         {user.name}
         <img src={user.image} alt={user.name} />
+      </div>
+
+      <div>
+        <h2>Users</h2>
+        <ul>
+          {usersData.map((u) => (
+            <li key={u.id}>
+              {/* biome-ignore lint/style/noNonNullAssertion: <explanation> */}
+              <img src={u.image!} alt={u.name!} />
+              {u.name}
+            </li>
+          ))}
+        </ul>
       </div>
 
       <Form action="/api/auth/signout" method="post">
